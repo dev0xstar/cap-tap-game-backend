e3e8ce9af507f53e1ac9f404ef753b19dc3e753f
Sayem98 <sayemabedin.bd@gmail.com>
2024-11-13 21:06:02 +0600

    exchange

--- src/controllers/Exchange.ts
+++ src/controllers/Exchange.ts
@@ -1,7 +1,70 @@
 import { Request, Response } from "express";
+import { Rates } from "../models/Rates";
+import { User } from "../models/User";
+export const exchange = async (req: Request, res: Response) => {
+  const { user, from, to, amount } = req.body;
+  const rate = await Rates.findOne({ title: "rates" });
 
-export const এক্সচেঞ্জ = async (req: Request, res: Response) => {
+  if (!rate) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+
+  // get user
+  const userObject = await User.findOne({ tg_id: user });
+  if (!userObject) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+
+  // check balance
+  if (from === "odp") {
+    if (userObject.odp < amount) {
+      return res.status(400).json({
+        message: "Insufficient balance",
+      });
+    }
+  } else if (from === "itp") {
+    if (userObject.mount < amount) {
+      return res.status(400).json({
+        message: "Insufficient balance",
+      });
+    }
+  } else {
+    return res.status(400).json({
+      message: "Invalid currency",
+    });
+  }
+
+  // calculate amount
+  let newAmount = 0;
+  if (from === "odp") {
+    newAmount = amount * rate.otp;
+  } else if (from === "otp") {
+    newAmount = amount * rate.odp;
+  }
+
+  // update user balance
+  if (from === "odp") {
+    userObject.odp -= amount;
+    userObject.balance += newAmount;
+  } else if (from === "otp") {
+    userObject.balance -= amount;
+    userObject.odp += newAmount;
+  }
+
+  await userObject.save();
+
   res.status(200).json({
-    message: "এক্সচেঞ্জ কন্ট্রোলারে আপনাকে স্বাগতম",
+    message: "success",
+    data: user,
+    exchangeData: {
+      from,
+      to,
+      amount,
+      newAmount,
+    },
   });
 };
